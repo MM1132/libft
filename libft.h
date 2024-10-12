77de61dcbb00bf8bb8296a45d427e65300fda545
Robert Reimann <rreimann@1-C-31.42heilbronn.de>
2024-10-12 21:45:54 +0200

    strrchr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 13:05:49 by rreimann          #+#    #+#             */
-/*   Updated: 2024/10/12 16:55:26 by rreimann         ###   ########.fr       */
+/*   Updated: 2024/10/12 21:43:26 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 #endif
