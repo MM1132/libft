398c2a0e8facba9d5bc4f3ad06b7f15054541b6a
Robert Reimann <rreimann@1-D-18.42heilbronn.de>
2024-10-12 12:57:57 +0200

    norminette: strlcpy

--- functions/ft_strlcpy.c
+++ functions/ft_strlcpy.c
@@ -6,26 +6,11 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 17:43:30 by rreimann          #+#    #+#             */
-/*   Updated: 2024/10/12 12:56:40 by rreimann         ###   ########.fr       */
+/*   Updated: 2024/10/12 12:57:42 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
-
-// "aaa", 3
-
-// "string"
-// "aaa"
-// 3
-
-// "aaring"
-// "aaa"
-// 3
-// 3 - 1 = 2
-// index: 2
-
-// "aasdjj;s;sa", 100
-
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
@@ -46,38 +31,3 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	dst[index] = 0;
 	return (src_len);
 }
-
-
-
-// size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-// {
-// 	size_t	index;
-
-// 	index = 0;
-// 	while (src[index] != 0 && index < dstsize - 1)
-// 	{
-// 		dst[index] = src[index];
-// 		index++;
-// 	}
-// 	if (dstsize > 0)
-// 	{
-// 		dst[index] = 0;
-// 	}
-// 	return (ft_strlen(src));
-// }
-
-/*
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-	unsigned int	index;
-
-	index = 0;
-	while (src[index] != 0 && index < size - 1)
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	dest[index] = '\0';
-	return (ft_strlen(src));
-}
-*/
